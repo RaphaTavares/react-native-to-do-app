a96a7f5b0907284a509b0669a5717d28e1962a96
RaphaelCoder <raphael_tavares2000@hotmail.com>
2021-09-26 16:40:50 -0300

    Add submit handler function to add todo

--- App.js
+++ App.js
@@ -1,39 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
 
 export default function App() {
-
   const [todos, setTodos] = useState([
     { text: 'buy coffee', key: '1' },
     { text: 'create an app', key: '2' },
-    { text: 'play on the switch', key: '3' }
+    { text: 'play on the switch', key: '3' },
   ]);
 
-  const pressHandler = key =>{
-    setTodos((prevTodos) => {
+  const pressHandler = (key) => {
+    setTodos(prevTodos => {
       return prevTodos.filter(todo => todo.key != key);
     });
-  }
+  };
+
+  const submitHandler = (text) => {
+    if(text.length > 3){
+      setText('');
+      setTodos(prevTodos => {
+        return [
+          { text, key: Math.random().toString() },
+          ...prevTodos
+        ];
+      });
+    } else {
+      Alert.alert('OOPS', 'Todo must be over 3 characters long', [
+        {text: 'Understood', onPress: () => console.log('alert closed') }
+      ]);
+    }
+  };
 
   return (
-    <View style={styles.container}>
-
-      <Header />
-      <View style={styles.content}>
-        {/*form*/}
-        <View style={styles.list}>
-          <FlatList 
-          data={todos}
-          renderItem={( { item } ) => (
-            <TodoItem item={item} pressHandler={pressHandler}/>
-            )}
-          />
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -41,13 +57,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#222222',
-    alignItems: 'center',
-    justifyContent: 'center',
+    color: 'white'
   },
   content: {
     padding: 40,
   },
-  list:{
+  list: {
     marginTop: 20,
   },
 });
